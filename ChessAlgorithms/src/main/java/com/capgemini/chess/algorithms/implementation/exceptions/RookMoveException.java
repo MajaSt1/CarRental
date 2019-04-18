c51eb9732f60dd020c0d9eb3ef3e8580096ae69f
Maja Stasinska <maja.stasinska@capgemini.com>
2019-04-18 12:53:32 +0200

    First commit with database

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/exceptions/RookMoveException.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.chess.algorithms.implementation.exceptions;
-
-public class RookMoveException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	 public RookMoveException() {
-		super("Cant move like that");
-	}
-}
